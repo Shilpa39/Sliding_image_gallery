6bc59e1721f5dadd730777ed06fb2b1409850a87
Shilpa39 <3876alaha@gmail.com>
2017-12-11 21:01:44 +0530

    img sliding left and right

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,32 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+this.ind=0;
 
   }
+
+  slide = [{ image: "assets/imgs/fruit1.jpg",num:1 },{ image: "assets/imgs/fruit2.jpg",num:2 }, { image: "assets/imgs/fruit3.jpg",num:3 },  { image: "assets/imgs/fruit4.jpg",num:4 }]
+
+ind: number;
+
+nextimg()
+{if(this.ind==3)
+ {
+   this.ind=0;
+ }
+else
+ {
+this.ind = this.ind+1;
+ }}
+
+previmg()
+{if(this.ind==0)
+ {
+   this.ind=3;
+ }
+else
+ {
+this.ind = this.ind-1;
+ }}
 
 }
