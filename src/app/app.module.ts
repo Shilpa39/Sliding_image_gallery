b63718f621e79c8a25246dc0853f6b560e140aa5
Shiv Tavker <tavkershiv@gmail.com>
2017-12-13 05:46:03 +0530

    Imported Plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {SocialSharing} from '@ionic-native/social-sharing';
+import {File} from '@ionic-native/file';
+import {FileTransfer} from '@ionic-native/file-transfer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,6 +30,9 @@ import {ModalPage} from '../pages/modal/modal';
   providers: [
     StatusBar,
     SplashScreen,
+    SocialSharing,
+    File,
+    FileTransfer,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
